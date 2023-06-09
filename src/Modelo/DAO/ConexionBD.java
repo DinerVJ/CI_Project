0c6244c68c54ccefb0db5098abec20bd92805cee
dinervj <dinervillavicenciojaimes@gmail.com>
2023-06-09 16:01:37 -0500

    Se ha intentado configurar la conexion a sql server, las lineas se comentaron

--- src/Modelo/DAO/ConexionBD.java
+++ src/Modelo/DAO/ConexionBD.java
@@ -3,6 +3,8 @@ package Modelo.DAO;
 import java.sql.*;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ConexionBD implements IntConexionBDDAO{
     
@@ -18,5 +20,15 @@ public class ConexionBD implements IntConexionBDDAO{
         }
         return cn;
         
+//        try {
+//            Class.forName(driver);
+//            cn = DriverManager.getConnection(url, user, password);
+//            System.out.println("Exito");
+//        } catch (SQLException e) {
+//            System.out.println("Error de conexion: " + e.getMessage());
+//        } catch (ClassNotFoundException ex) {
+//            Logger.getLogger(ConexionBD.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        return cn;
     }
 }
--- src/Principal/Principal.java
+++ src/Principal/Principal.java
@@ -1,5 +1,7 @@
 package Principal;
 
+import Controlador.ControladorLogin;
+//import Modelo.DAO.ConexionBD;
 import Vista.VistaLogin;
 
 public class Principal {
@@ -10,10 +12,12 @@ public class Principal {
     public static void main(String[] args) {
         // TODO code application logic here
         VistaLogin vl = new VistaLogin();
-        //ControladorLogin cl = new ControladorLogin(vl);
-        //cl.iniciar();
+        ControladorLogin cl = new ControladorLogin(vl);
+        cl.iniciar();
         vl.setVisible(true);
-        //vl.setLocationRelativeTo(null);
+        vl.setLocationRelativeTo(null);
+//        ConexionBD con = new ConexionBD();
+//        con.conectar();
     }
     
 }
